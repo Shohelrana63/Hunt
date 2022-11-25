c98f85a060c35d5a178d41894e3ffa0c7f59f7f7
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2022-11-26 01:10:02 +0600

    Navbar & Banner done

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(function() {
+    $('.banner-item').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        // autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: false,
+        dots: false,
+      });
+
+          
+})
