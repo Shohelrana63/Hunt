6fd66224ec5a194657830110add757c43e470f68
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2022-12-02 21:13:09 +0600

    service-part done

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
-$(function() {
-    $('.banner-item').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        // autoplay: true,
-        autoplaySpeed: 2000,
-        arrows: false,
-        dots: false,
-      });
+$(function () {
+  $('.banner-item').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: false,
+    dots: false,
+  });
 
-          
+  $('.service-main').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    centerMode: true,
+    centerPadding: '0px',
+    nextArrow : '.up',
+    prevArrow : '.down',
+    vertical: true,
+  });
+
+
+
 })
