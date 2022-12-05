1c278fbcc597f7e467c007ba00352140ee4f20a8
Shohel Rana <39130772+Shohelrana63@users.noreply.github.com>
2022-12-05 23:57:20 +0600

    team-part done with slider

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ $(function () {
   $('.service-main').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: true,
+    // autoplay: true,
     autoplaySpeed: 2000,
     centerMode: true,
     centerPadding: '0px',
@@ -19,7 +19,29 @@ $(function () {
     prevArrow : '.down',
     vertical: true,
   });
+  $('.item-slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    // autoplay: true,
+    autoplaySpeed: 2000,
+    vertical: true,
+    nextArrow: '<i class="fas fa-chevron-up team-up"></i>',
+    prevArrow: '<i class="fas fa-chevron-down team-down"></i>',
+    asNavFor: '.details-slider',
+    centerMode: true,
+  centerPadding: "0"
+  });
 
-
+  $('.details-slider').slick({
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    focusOnSelect: true,
+    asNavFor: '.item-slider',
+  });
 
 })
